b7157d9df301b215f96274f262dfd7716dda2464
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-05-20 10:29:14 +0300

    Add git-signs conf

---
+++ lua/plugins/git-signs.lua
@@ -0,0 +1,9 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  keys = {
+    { mode = { "n" }, "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle current line blame" },
+  },
+  opts = {
+    numhl = true,
+  },
+}
