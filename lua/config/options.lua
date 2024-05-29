d2cfb07e796bbbe933347be8c06089d0e6c6e402
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-05-29 17:12:11 +0300

    Decrease scrolloff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 5 -- Minimal number of screen lines to keep above and below the cursor.
 
 vim.opt.shell = "nu"
 
