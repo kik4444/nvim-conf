09230a0b16b9e21131eeb460e594062ac8b6848f
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-06-03 16:24:13 +0300

    Enlargen telescope window and other options

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,5 +5,16 @@ return {
       { "<leader>fb", false }, -- Disable default search in buffer
       { "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "[/] Fuzzily search in current buffer" },
     },
+    opts = {
+      defaults = {
+        wrap_results = true,
+        scroll_strategy = "limit",
+        layout_config = {
+          width = 0.99,
+          height = 0.95,
+          preview_width = 0.65,
+        },
+      },
+    },
   },
 }
