90ae92be2351f5ca41158f107d987d87596afc23
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-07-03 11:39:06 +0300

    Disable tokyonight too

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,6 @@
 return {
   { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
   { "Mofiqul/vscode.nvim", priority = 1000, lazy = false },
   {
     "LazyVim/LazyVim",
