9d4f6ddfc6c13de982e63830d5af667802918c42
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-08-02 14:46:38 +0300

    Add SQL extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+
+    { import = "lazyvim.plugins.extras.lang.sql" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
