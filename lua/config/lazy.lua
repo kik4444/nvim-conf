443bb6c5fd5001aeff114a92a1063016db6d6d66
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-12-12 10:55:49 +0200

    Update and switch to blink

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
 
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.blink" },
 
     { import = "lazyvim.plugins.extras.util.startuptime" },
 
