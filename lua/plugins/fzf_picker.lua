7fbc94e629450d18e47885da08d061459669314c
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-12-18 20:11:38 +0200

    Add opening multiple selections in fzf

--- lua/plugins/fzf_picker.lua
+++ lua/plugins/fzf_picker.lua
@@ -3,5 +3,8 @@ return {
   opts = function(_, opts)
     opts.winopts.width = 0.99
     opts.winopts.preview.horizontal = "right:65%"
+
+    -- Open multiple selections in buffers instead of sending them to quickfix list.
+    require("fzf-lua.config").defaults.actions.files.default = require("fzf-lua.actions").file_edit
   end,
 }
