bdd0fe72e9a5c6cf7b2266506a83454e419d190f
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-12-19 09:28:12 +0200

    Picker remap grep in buffer

--- lua/plugins/fzf_picker.lua
+++ lua/plugins/fzf_picker.lua
@@ -1,5 +1,8 @@
 return {
   "ibhagwan/fzf-lua",
+  keys = {
+    { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
+  },
   opts = function(_, opts)
     opts.winopts.width = 0.99
     opts.winopts.preview.horizontal = "right:65%"
